ebb5e69cf5bf524524deb878dbc3317111fc64b9
irajeshh <i.rajeshh@hotmail.com>
2022-11-27 12:39:00 +0530

    Web scroll bug fixed

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fluttership/Constants/Constants.dart';
+import 'package:fluttership/Scroll.dart';
 import 'package:oktoast/oktoast.dart';
 import 'UI/Home/Home.dart';
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return OKToast(
       child: MaterialApp(
+        scrollBehavior: WeBScrollBehavior(),
         debugShowCheckedModeBanner: false,
         home: HomePage(),
         theme: theme,
@@ -19,17 +20,7 @@ class MyApp extends StatelessWidget {
   }
 
   ThemeData get theme => ThemeData.light().copyWith(
-        //whatever the themedata is & whatever the phone theme is
-        brightness: Brightness.light, // set brightness to dark
-        //now we need to match the blue colors, let us pick it from
-        //photoshop
-        //Press PrintScreen to copy the current window
-        //Open Photoshop
-        //Press Ctrl +N
-        //Press Ctrl +V
-        //Now click on the specific color you want
-        //find the color code
-        //past the code in flutter
-        primaryColor: Colorz.backgroundBlue,
+        brightness: Brightness.light,
+        primaryColor: Colorz.primaryColor,
       );
 }
---
+++ lib/Scroll.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class WeBScrollBehavior extends MaterialScrollBehavior {
+  // Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.stylus,
+        PointerDeviceKind.invertedStylus,
+        PointerDeviceKind.trackpad,
+      };
+}
