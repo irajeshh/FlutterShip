912cf8376d1c2bb7cf429c288b084a6f99896b99
irajeshh <i.rajeshh@hotmail.com>
2022-11-27 13:00:50 +0530

    Mobile home page completed

--- lib/UI/Home/Mobile.dart
+++ lib/UI/Home/Mobile.dart
@@ -16,6 +16,7 @@ class MobileHomePage extends StatefulWidget {
 class _MobileHomePageState extends State<MobileHomePage> {
   final TextEditingController searchController = TextEditingController();
   String query = '';
+  bool useGridView = false;
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +31,7 @@ class _MobileHomePageState extends State<MobileHomePage> {
       title: Txt(
         text: 'Fluttershipp',
       ),
+      centerTitle: false,
       backgroundColor: Colorz.primaryColor,
     );
   }
@@ -37,60 +39,148 @@ class _MobileHomePageState extends State<MobileHomePage> {
   Widget body() {
     return Column(
       children: [
-        searchField(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            styleButton(),
+            searchField(),
+          ],
+        ),
         clonesResult(),
       ],
     );
   }
 
   Widget searchField() {
-    return SearchBar(
-      onChanged: (z) => setState(() => query = z.toLowerCase()),
+    return Expanded(
+      child: SearchBar(
+        onChanged: (z) => setState(() => query = z.toLowerCase()),
+      ),
     );
   }
 
   Widget clonesResult() {
-    List<Clone> _clones = widget.clones.where((clone) {
-      return clone.title.toString().toLowerCase().contains(query) ||
-          clone.title.toString().toLowerCase().contains(query);
-    }).toList();
     return Expanded(
-      child: ListView.builder(
-        shrinkWrap: false,
-        itemCount: _clones.length,
-        itemBuilder: (BuildContext context, int index) {
-          final Clone clone = _clones[index];
-          return cloneTile(clone);
-        },
+      child: CrossFade(
+        useCenter: false,
+        hiddenChild: listViewBuilder(),
+        show: useGridView,
+        child: gridViewBuilder(),
       ),
     );
   }
 
-  Widget cloneTile(Clone clone){
+  Widget listViewBuilder() {
+    return ListView.builder(
+      itemCount: _clones.length,
+      itemBuilder: (BuildContext context, int index) {
+        final Clone clone = _clones[index];
+        return cloneTile(clone);
+      },
+    );
+  }
+
+  Widget gridViewBuilder() {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 12,
+        mainAxisSpacing: 16,
+      ),
+      itemCount: _clones.length,
+      itemBuilder: (BuildContext context, int index) {
+        final Clone clone = _clones[index];
+        return cloneCard(clone);
+      },
+    );
+  }
+
+  Widget cloneTile(Clone clone) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 12,),
+      padding: const EdgeInsets.symmetric(
+        vertical: 12,
+      ),
       child: Card(
         elevation: 6,
         child: ListTile(
-                leading: IconButton(
-                    icon: Icon(
-                  clone.icon,
-                  color: clone.color,
-                )),
-                title: Txt(
-                  text: clone.title,
-                  fontWeight: FontWeight.bold,
-                ),
-                subtitle: Txt(
-                  text: clone.description,
-                ),
-                onTap: () {
-                  Widgets.push(clone.page, context);
-                },
-              ),
+          leading: IconButton(
+              icon: Icon(
+            clone.icon,
+            color: clone.color,
+          )),
+          title: Txt(
+            text: clone.title,
+            fontWeight: FontWeight.bold,
+          ),
+          subtitle: Txt(
+            text: clone.description,
+          ),
+          onTap: () {
+            Widgets.push(clone.page, context);
+          },
+        ),
       ),
     );
   }
+
+  Widget cloneCard(Clone clone) {
+    return Card(
+      elevation: 8,
+      shadowColor: clone.color.withAlpha(100),
+      child: Container(
+        color: clone.color.withOpacity(0.1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              clone.icon,
+              color: clone.color,
+              size: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Txt(
+                text: clone.title,
+                maxLines: 1,
+                fontSize: 18,
+                color: clone.color,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget styleButton() {
+    return IconButton(
+      icon: CrossFade(
+        show: useGridView,
+        hiddenChild: const Icon(
+          Icons.grid_view_rounded,
+          color: Colorz.primaryColor,
+        ),
+        child: const RotatedBox(
+          quarterTurns: 1,
+          child: Icon(
+            Icons.view_week,
+            color: Colorz.primaryColor,
+          ),
+        ),
+      ),
+      tooltip: !useGridView ? 'Change to grid view' : 'Change to list view',
+      onPressed: () {
+        if (mounted) setState(() => useGridView = !useGridView);
+      },
+    );
+  }
+
+  List<Clone> get _clones => widget.clones.where((clone) {
+        return clone.title.toString().toLowerCase().contains(query) ||
+            clone.title.toString().toLowerCase().contains(query);
+      }).toList();
 
   void clearSearch() {
     searchController.clear();
