285e1251f386628abea36766348a168655b18ac1
irajeshh <i.rajeshh@hotmail.com>
2022-11-27 13:58:52 +0530

    Staggered grid view removed

--- lib/UI/Pages/InterLacedDashBoard.dart
+++ lib/UI/Pages/InterLacedDashBoard.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:fluttership/Constants/Constants.dart';
 import 'package:fluttership/Models/IconLabel.dart';
 import 'package:fluttership/Models/InterlacedEvent.dart';
